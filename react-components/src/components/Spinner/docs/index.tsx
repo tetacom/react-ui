90085e908cacf39208151c394c97767dd4d67881
shtirlizc <shtirlizc@gmail.com>
2023-05-11 10:44:43 +0500

    update docs markup with Stack component

--- react-components/src/components/Spinner/docs/index.tsx
+++ react-components/src/components/Spinner/docs/index.tsx
@@ -12,6 +12,7 @@ import { Typography } from '../../Typography';
 import { Button } from '../../Button';
 import { ButtonProps } from '../../Button/model';
 import { Icon } from '../../Icons';
+import { Stack } from '../../Stack';
 
 import s from './style.module.scss';
 import spinnerClassNames from '../style.module.scss';
@@ -53,77 +54,75 @@ const exampleButtons: (Pick<ButtonProps, 'view' | 'square'> & {
   },
 ];
 
-export const SpinnerDocs = () => {
-  return (
-    <Unstyled>
-      <div className={s.root}>
-        <Title
-          level={1}
-          fontVariant="h4"
-          style={{
-            marginBottom: 0,
-          }}
-        >
-          Спиннер
-        </Title>
-        <Paragraph
-          fontVariant="caption"
-          style={{ marginTop: 0, color: 'var(--color-primary-50)' }}
-        >
-          {spinnerClassNames.spinner}
-        </Paragraph>
+export const SpinnerDocs = () => (
+  <Unstyled>
+    <div className={s.root}>
+      <Title
+        level={1}
+        fontVariant="h4"
+        style={{
+          marginBottom: 0,
+        }}
+      >
+        Спиннер
+      </Title>
+      <Paragraph
+        fontVariant="caption"
+        style={{ marginTop: 0, color: 'var(--color-primary-50)' }}
+      >
+        {spinnerClassNames.spinner}
+      </Paragraph>
 
-        <br />
+      <br />
 
-        <Paragraph>
-          Spinner (спинер) — это зацикленный индикатор, не отображающий прогресс
-          выполнения задачи.
-        </Paragraph>
+      <Paragraph>
+        Spinner (спинер) — это зацикленный индикатор, не отображающий прогресс
+        выполнения задачи.
+      </Paragraph>
 
-        <Title level={2} fontVariant="h6">
-          Как использовать
-        </Title>
-        <Paragraph>
-          Не показывайте на странице сразу несколько спиннеров, даже если они
-          иллюстрируют не связанные процессы. Это создает неприятное мельтешение
-          и излишне акцентирует внимание пользователя на процессе загрузки. Как
-          правило, есть другой способ показать процесс загрузки.
-        </Paragraph>
-        <Paragraph>Чаще всего применяется на кнопках.</Paragraph>
+      <Title level={2} fontVariant="h6">
+        Как использовать
+      </Title>
+      <Paragraph>
+        Не показывайте на странице сразу несколько спиннеров, даже если они
+        иллюстрируют не связанные процессы. Это создает неприятное мельтешение и
+        излишне акцентирует внимание пользователя на процессе загрузки. Как
+        правило, есть другой способ показать процесс загрузки.
+      </Paragraph>
+      <Paragraph>Чаще всего применяется на кнопках.</Paragraph>
 
-        <div className={s.exampleBlock}>
-          {[
-            'var(--color-primary-50)',
-            'var(--color-text-30)',
-            'var(--color-global-white)',
-          ].map((spin) => (
-            <div key={spin}>
-              <Spinner color={spin} size={32} />
-            </div>
-          ))}
-        </div>
+      <Stack size={32} className={s.exampleBlock}>
+        {[
+          'var(--color-primary-50)',
+          'var(--color-text-30)',
+          'var(--color-global-white)',
+        ].map((spin) => (
+          <div key={spin}>
+            <Spinner color={spin} size={32} />
+          </div>
+        ))}
+      </Stack>
 
-        <Title level={2} fontVariant="h4">
-          Принцип работы
-        </Title>
+      <Title level={2} fontVariant="h4">
+        Принцип работы
+      </Title>
 
-        <div className={s.exampleUsageBlock}>
-          {exampleButtons.map(({ id, view, square }) => (
-            <div key={id}>
-              <Button size="large" view={view} square={square} loading>
-                Загрузить
-                <Icon name="user" />
-              </Button>
-            </div>
-          ))}
-        </div>
+      <Stack size={96} wrap style={{ margin: '24px 0' }}>
+        {exampleButtons.map(({ id, view, square }) => (
+          <div key={id}>
+            <Button size="large" view={view} square={square} loading>
+              Загрузить
+              <Icon name="user" />
+            </Button>
+          </div>
+        ))}
+      </Stack>
 
-        <br />
-        <br />
-        <Primary />
-        <ArgsTable story={PRIMARY_STORY} />
-        <Stories />
-      </div>
-    </Unstyled>
-  );
-};
+      <br />
+      <br />
+      <Primary />
+      <ArgsTable story={PRIMARY_STORY} />
+      <Stories />
+    </div>
+  </Unstyled>
+);
