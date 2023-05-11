997cf35cdf5bb041417f542684b678eb342aeb98
shtirlizc <shtirlizc@gmail.com>
2023-05-11 11:41:30 +0500

    add descriptions to tabs api

--- react-components/src/components/Tabs/model/index.ts
+++ react-components/src/components/Tabs/model/index.ts
@@ -3,9 +3,18 @@ import { TabType } from './tabType';
 type Direction = 'top' | 'left';
 
 export interface TabsProps {
+  // Настроить содержимое вкладок
   items: TabType[];
+
+  // Ключ исходной активной вкладки, если activeKey не установлен
   defaultActiveKey?: TabType['key'];
+
+  // Ключ текущий вкладки ключ
   activeKey?: TabType['key'];
+
+  // Обратный вызов выполняется при изменении активной вкладки
   onChange?: (activeKey: TabType['key']) => void;
+
+  // Направление вкладок
   direction?: Direction;
 }
--- react-components/src/components/Tabs/model/tabType.ts
+++ react-components/src/components/Tabs/model/tabType.ts
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export type TabType = {
+  // Уникальный идентификатор вкладки
   key: string;
+
+  // Текст заголовка вкладки
   label: string | React.ReactElement;
+
+  // Содержимое вкладки
   children?: string | React.ReactElement;
+
+  // Неактивная вкладка
   disabled?: boolean;
-  icon?: React.ReactElement;
 };
