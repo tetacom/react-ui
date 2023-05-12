575aa76b3eb74bf6dc2d05817ec5a687d027fcc8
shtirlizc <shtirlizc@gmail.com>
2023-05-12 13:35:42 +0500

    add docs

--- react-components/src/components/Avatar/docs/index.tsx
+++ react-components/src/components/Avatar/docs/index.tsx
@@ -7,121 +7,131 @@ import {
 } from '@storybook/addon-docs';
 import { Unstyled } from '@storybook/blocks';
 
-import { Chip } from '../index';
+import { Avatar } from '../index';
 import { Typography } from '../../Typography';
-import { ChipProps } from '../model';
-import { Icon } from '../../Icons';
+import { Stack } from '../../Stack';
 import img from './assets/avatar.jpg';
 
-import s from './style.module.scss';
-import chipClassNames from '../style.module.scss';
+import avatarClassNames from '../style.module.scss';
 
-const { Title, Paragraph } = Typography;
+const { Title, Paragraph, Text } = Typography;
 
-type ExampleType = {
-  id: string;
-} & Pick<ChipProps, 'view' | 'picture' | 'icon'>;
-
-const chips: ExampleType[] = [
-  {
-    id: 'default-with-picture',
-    view: 'default',
-    picture: <img src={img} alt="" />,
-  },
-  {
-    id: 'default-with-icon',
-    view: 'default',
-    icon: <Icon name="tick" />,
-  },
-  {
-    id: 'default',
-    view: 'default',
-  },
-
-  {
-    id: 'primary-with-picture',
-    view: 'primary',
-    picture: <img src={img} alt="" />,
-  },
-  {
-    id: 'primary-with-icon',
-    view: 'primary',
-    icon: <Icon name="tick" />,
-  },
-  {
-    id: 'primary',
-    view: 'primary',
-  },
-
-  {
-    id: 'outline-with-picture',
-    view: 'outline',
-    picture: <img src={img} alt="" />,
-  },
-  {
-    id: 'outline-with-icon',
-    view: 'outline',
-    icon: <Icon name="tick" />,
-  },
-  {
-    id: 'outline',
-    view: 'outline',
-  },
-];
-
-export const ChipDocs = () => {
+export const AvatarDocs = () => {
   return (
     <Unstyled>
-      <div className={s.root}>
-        <Title
-          level={1}
-          fontVariant="h4"
-          style={{
-            marginBottom: 0,
-          }}
-        >
-          Чип
-        </Title>
-        <Paragraph
-          fontVariant="caption"
-          style={{ marginTop: 0, color: 'var(--color-primary-50)' }}
-        >
-          {chipClassNames.chip}
-        </Paragraph>
+      <Title
+        level={1}
+        fontVariant="h4"
+        style={{
+          marginBottom: 0,
+        }}
+      >
+        Аватар
+      </Title>
+      <Paragraph
+        fontVariant="caption"
+        style={{ marginTop: 0, color: 'var(--color-primary-50)' }}
+      >
+        {avatarClassNames.avatar}
+      </Paragraph>
 
-        <br />
+      <br />
 
-        <Paragraph>
-          Чип — это компактные элементы, представляющие ввод, атрибут или
-          действие.
-        </Paragraph>
-        <Paragraph>
-          Чипы позволяют пользователям вводить информацию, делать выбор,
-          фильтровать содержимое или запускать действия.
-        </Paragraph>
-        <Paragraph>
-          Несмотря на то, что он включен сюда как отдельный компонент, наиболее
-          распространенное использование будет в той или иной форме ввода,
-          поэтому некоторые продемонстрированные здесь действия не показаны в
-          контексте.
-        </Paragraph>
+      <Paragraph>
+        Аватар — графическое изображение пользователя, фотография или рисунок.
+        Он нужен, чтобы быстро узнавать пользователя (например, в ветке
+        комментариев), а ещё — чтобы пользователь мог узнать сам себя. Например,
+        проверить, что вошёл в аккаунт под нужным логином.
+      </Paragraph>
+      <Paragraph>
+        В библиотеке сущесвтует два вида аватаров: изображения и цветные
+        буквенные. Также, они различаются по размерам и скруглениям.
+      </Paragraph>
 
-        <div className={s.exampleBlock}>
-          {chips.map(({ id, view, picture, icon }) => (
-            <div key={id}>
-              <Chip view={view} picture={picture} icon={icon} closable>
-                Floyd Miles
-              </Chip>
-            </div>
-          ))}
-        </div>
+      <Title level={2} fontVariant="h6">
+        Как использовать
+      </Title>
+      <Paragraph>Аватар нужен в случаях:</Paragraph>
+      <ul>
+        <Text>
+          <li>Если на странице есть возможность добавить комментарий;</li>
+        </Text>
+        <Text>
+          <li>Отображение профиля в хедере или же в других компонентах.</li>
+        </Text>
+      </ul>
+      <Paragraph>
+        В первом случае, при наведении появляется хинт, который показывает, кто
+        именно оставил комментарий. Во втором случае, при нажатии происходит
+        переход на страницу профиля или же вызывает выпадающий список с
+        навигацией.
+      </Paragraph>
 
-        <br />
-        <br />
-        <Primary />
-        <ArgsTable story={PRIMARY_STORY} />
-        <Stories />
-      </div>
+      <Title level={2} fontVariant="h6">
+        Скругления и размеры
+      </Title>
+      <Paragraph>
+        В основном, в наших продуктах используются круглые аватары (type: round)
+        с размером 28x28.
+      </Paragraph>
+      <Stack size={16} style={{ margin: '24px 0' }} block>
+        <Avatar name="МД" size="64" />
+        <Avatar name="МС" size="64" shape="brick" />
+        <Avatar name="яя" size="64" shape="round" />
+      </Stack>
+
+      <Stack size={20} align="start" style={{ margin: '24px 0' }}>
+        <Stack direction="column" size={4}>
+          <Avatar name="МД" size="26" picture={img} />
+          <Text fontVariant="body2">26px</Text>
+        </Stack>
+        <Stack direction="column" size={4}>
+          <Avatar name="МД" size="28" picture={img} />
+          <Text fontVariant="body2">28px</Text>
+        </Stack>
+        <Stack direction="column" size={4}>
+          <Avatar name="МД" size="32" picture={img} />
+          <Text fontVariant="body2">32px</Text>
+        </Stack>
+        <Stack direction="column" size={4}>
+          <Avatar name="МД" size="44" picture={img} />
+          <Text fontVariant="body2">44px</Text>
+        </Stack>
+        <Stack direction="column" size={4}>
+          <Avatar name="МД" size="64" picture={img} />
+          <Text fontVariant="body2">64px</Text>
+        </Stack>
+        <Stack direction="column" size={4}>
+          <Avatar name="МД" size="128" picture={img} />
+          <Text fontVariant="body2">128px</Text>
+        </Stack>
+        <Stack direction="column" size={4}>
+          <Avatar name="МД" size="200" picture={img} />
+          <Text fontVariant="body2">200px</Text>
+        </Stack>
+      </Stack>
+
+      <Title level={2} fontVariant="h6">
+        Цвета
+      </Title>
+      <Paragraph>
+        Цвет генерируется на основе переданного имени, т. е. каждый раз для
+        одного и того же имени будет один и тот же цвет.
+      </Paragraph>
+      <Paragraph>
+        На каждый цвет накладывается градиент:{' '}
+        <Text fontVariant="title2">
+          linear-gradient(180deg, rgba(255, 255, 255, 0.24) 0%, rgba(255, 255,
+          255, 0) 100%);
+        </Text>
+      </Paragraph>
+      <Paragraph></Paragraph>
+
+      <br />
+      <br />
+      <Primary />
+      <ArgsTable story={PRIMARY_STORY} />
+      <Stories />
     </Unstyled>
   );
 };
