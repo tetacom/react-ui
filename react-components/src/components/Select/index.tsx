a3b774cf61c19e1ded98f53d513bffd714dd4109
andy <andreyelpaev@gmail.com>
2023-06-20 10:20:31 +0500

    fix(library): add onItemRender callback for customize

--- react-components/src/components/Select/index.tsx
+++ react-components/src/components/Select/index.tsx
@@ -1,69 +1,83 @@
-import React, { forwardRef, useRef, useState } from 'react';
-import { SelectProps } from './model';
+import React, { forwardRef, ReactElement, Ref, useRef, useState } from 'react';
+import { BaseSelectProps, SelectProps } from './model';
 import { Dropdown, Icon, Input } from 'tetacom/react-components';
 import { useMergeRefs } from '@floating-ui/react';
 import s from './style.module.scss';
 import listStyle from '../List/style.module.scss';
 import classNames from 'classnames';
 
-export const Select = forwardRef<any, SelectProps>((props, ref) => {
-  const [value, setValue] = useState<any>(props.value);
-  const [open, setOpen] = useState<boolean>(false);
-  const inputRef = useRef<HTMLInputElement>(null);
+const SelectInner = forwardRef(
+  <T extends BaseSelectProps>(props: SelectProps<T>, ref: any) => {
+    const [value, setValue] = useState<any>(props.value);
+    const [open, setOpen] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null);
 
-  const foundValue = props.items?.find((item) => item === value);
+    const foundValue = props.items?.find((item) => item === value);
 
-  return (
-    <Dropdown
-      possiblePlacements={['bottom', 'top']}
-      placement="bottom"
-      autoWidth={true}
-      resizable={false}
-      open={props.disabled ? false : open}
-      onOpenChange={(e) => !props.disabled && setOpen(e)}
-      dropdown={
-        <ul className={listStyle.list}>
-          <>
-            {props.items?.map((item, index) => (
-              <li
-                key={item.key}
-                role="option"
-                className={classNames([
-                  listStyle.item,
-                  item === foundValue ? s.selected : null,
-                ])}
-                onClick={() => {
-                  setValue(item);
-                  setOpen(false);
-                  props.onClick && props.onClick(item);
-                }}
-              >
-                <span className={listStyle.textHeadline}>{item.headline}</span>
-              </li>
-            ))}
-          </>
-        </ul>
-      }
-    >
-      <div style={{ position: 'relative' }}>
-        <Input
-          {...props}
-          ref={useMergeRefs([inputRef, ref])}
-          value={foundValue?.headline}
-          readonly
-        ></Input>
-        <div className={s.arrow}>
-          <Icon
-            style={{
-              color: props.disabled
-                ? 'var(--color-text-20)'
-                : 'var(--color-text-50)',
-              transform: open ? 'rotate(180deg)' : 'rotate(0deg)',
-            }}
-            name={'arrowDownKey'}
-          />
+    return (
+      <Dropdown
+        possiblePlacements={['bottom', 'top']}
+        placement="bottom"
+        autoWidth={true}
+        resizable={false}
+        open={props.disabled ? false : open}
+        onOpenChange={(e) => !props.disabled && setOpen(e)}
+        dropdown={
+          <ul className={listStyle.list}>
+            <>
+              {props.items?.map((item, index) => (
+                <li
+                  key={item.key}
+                  role="option"
+                  className={classNames([
+                    listStyle.item,
+                    item === foundValue ? s.selected : null,
+                  ])}
+                  onClick={(event) => {
+                    setValue(item);
+                    setOpen(false);
+                    props.onChangeItem && props.onChangeItem(item);
+                  }}
+                >
+                  {props.onItemRender ? (
+                    <div className={listStyle.textHeadline}>
+                      {props.onItemRender(item)}
+                    </div>
+                  ) : (
+                    <span className={listStyle.textHeadline}>
+                      {item.headline}
+                    </span>
+                  )}
+                </li>
+              ))}
+            </>
+          </ul>
+        }
+      >
+        <div style={{ position: 'relative' }}>
+          <Input
+            {...props}
+            ref={useMergeRefs([inputRef, ref])}
+            value={foundValue?.headline}
+            readonly
+          ></Input>
+          <div className={s.arrow}>
+            <Icon
+              style={{
+                color: props.disabled
+                  ? 'var(--color-text-20)'
+                  : 'var(--color-text-50)',
+                transform: open ? 'rotate(180deg)' : 'rotate(0deg)',
+              }}
+              name={'arrowDownKey'}
+            />
+          </div>
         </div>
-      </div>
-    </Dropdown>
-  );
-});
+      </Dropdown>
+    );
+  },
+);
+
+export const Select = SelectInner as <T extends BaseSelectProps>(
+  props: SelectProps<T> & { ref: Ref<any> },
+) => JSX.Element;
