01c7d4b4a57f76a303c238bf11b6868baaf988cf
shtirlizc <shtirlizc@gmail.com>
2023-07-25 12:37:18 +0500

    fix(style): update stack and add justify-content prop

--- react-components/src/components/Stack/model/index.ts
+++ react-components/src/components/Stack/model/index.ts
@@ -1,13 +1,22 @@
 import React, { CSSProperties, PropsWithChildren } from 'react';
 
 type DirectionType = 'row' | 'row-reverse' | 'column' | 'column-reverse';
+type JustifyContent =
+  | 'flex-start'
+  | 'center'
+  | 'flex-end'
+  | 'space-between'
+  | 'stretch';
 type AlignType = 'stretch' | 'start' | 'end' | 'center' | 'baseline';
 
 export interface StackProps extends PropsWithChildren {
   // Направление
   direction?: DirectionType;
 
-  // Выровнять элементы
+  // Выровнять элементы вдоль напправления
+  justifyContent?: JustifyContent;
+
+  // Выровнять элементы поперект направления
   align?: AlignType;
 
   // Расстояние между элементами
@@ -24,5 +33,7 @@ export interface StackProps extends PropsWithChildren {
 
   // Дополнительный класс для Stack
   className?: string;
+
+  // Встроенные стили
   style?: CSSProperties;
 }
