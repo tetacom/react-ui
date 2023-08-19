66294e11e5b5105a06c46dd66d23d6133bc1feae
shtirlizc <shtirlizc@gmail.com>
2023-08-19 12:01:23 +0500

    fix: exclude some field from localstorage

--- react-components/src/components/Table/storageUtils.ts
+++ react-components/src/components/Table/storageUtils.ts
@@ -1,7 +1,22 @@
 import type { TableColumn } from './model/table-column';
 
+// TODO подумать как переделать
+
 export type ColumnWidth = { name: string; width: number };
-type StorageTableColumn = Omit<TableColumn, 'cellComponent'>;
+type StorageTableColumn = Omit<
+  TableColumn,
+  | 'cellComponent'
+  | 'caption'
+  | 'hint'
+  | 'sortable'
+  | 'sortField'
+  | 'editable'
+  | 'filterable'
+  | 'filterType'
+  | 'filterField'
+  | 'required'
+  | 'sortOrder'
+>;
 
 export function mergeSettings(
   propData: TableColumn[],
@@ -12,9 +27,35 @@ export function mergeSettings(
       const savedColumn: TableColumn | undefined = storageData.find(
         ({ name }) => name === propColumn.name,
       );
+      const {
+        cellComponent,
+        caption,
+        hint,
+        sortable,
+        sortField,
+        editable,
+        filterable,
+        filterType,
+        filterField,
+        required,
+        sortOrder,
+      } = propColumn;
 
       if (savedColumn) {
-        return { ...savedColumn, cellComponent: propColumn.cellComponent };
+        return {
+          ...savedColumn,
+          cellComponent,
+          caption,
+          hint,
+          sortable,
+          sortField,
+          editable,
+          filterable,
+          filterType,
+          filterField,
+          required,
+          sortOrder,
+        };
       }
     }
 
@@ -26,9 +67,25 @@ export function separateSettings(
   dataToSave: TableColumn[],
   columnsWidth: ColumnWidth[],
 ): StorageTableColumn[] {
-  return dataToSave.map(({ cellComponent, ...rest }) => ({
-    ...rest,
-    width:
-      columnsWidth.find(({ name }) => name === rest.name)?.width ?? rest.width,
-  }));
+  return dataToSave.map(
+    ({
+      cellComponent,
+      caption,
+      hint,
+      sortable,
+      sortField,
+      editable,
+      filterable,
+      filterType,
+      filterField,
+      required,
+      sortOrder,
+      ...rest
+    }) => ({
+      ...rest,
+      width:
+        columnsWidth.find(({ name }) => name === rest.name)?.width ??
+        rest.width,
+    }),
+  );
 }
