2287b9976047ce6963fbfefe6588f6bc8e00f0ac
shtirlizc <shtirlizc@gmail.com>
2023-10-16 12:10:02 +0500

    fix: change table header bg

--- react-components/src/components/Table/stories/Table.stories.tsx
+++ react-components/src/components/Table/stories/Table.stories.tsx
@@ -8,14 +8,15 @@ import { FilterType } from '../model/public-api';
 import { Toggle } from '../../Toggle';
 import { ICellInstance } from '../model/i-cell-instance';
 import { IDictionary } from '../model/dictionary';
-
-import configResponse from './configResponse.json';
-import dataResponse from './dataResponse.json';
-import dictResponse from './dictResponse.json';
 import { CellParamsType } from '../model/cell-params';
 import { Skeleton } from '../../Skeleton';
 import { Typography } from '../../Typography';
 import { ICellComponent } from '../model/i-cell-component';
+import { Card } from '../../Card';
+
+import configResponse from './configResponse.json';
+import dataResponse from './dataResponse.json';
+import dictResponse from './dictResponse.json';
 
 const { Paragraph } = Typography;
 
@@ -92,12 +93,22 @@ customComponents.set(FilterType.boolean, CustomComponentWithToggle);
 customComponents.set(FilterType.date, CustomComponentWithDate);
 
 const TableStory: FC<{
+  localStorageKey?: string;
   sticky?: boolean;
   loading?: boolean;
   cellParams?: CellParamsType;
   height?: React.CSSProperties['height'];
+  hiddenColumnNames?: string[];
   acrossLine?: boolean;
-}> = ({ sticky = false, loading = false, cellParams, height, acrossLine }) => {
+}> = ({
+  localStorageKey,
+  sticky = false,
+  loading = false,
+  cellParams,
+  height,
+  acrossLine,
+  hiddenColumnNames,
+}) => {
   const columns = initColumns.map((column) => {
     const { name, filterType } = column;
 
@@ -123,39 +134,71 @@ const TableStory: FC<{
   };
 
   return (
-    <Table
-      localStorageKey="sb"
-      height={height}
-      acrossLine={acrossLine}
-      dataSource={dataResponse}
-      columns={columns}
-      sticky={sticky}
-      skeleton={
-        loading ? (
-          <Skeleton
-            rows={16}
-            columns={[2, 3, 5, 10, 3, 16, 6, 9, 9, 7, 8, 10, 10]}
-            columnsUnit="fr"
-            isTable
-          />
-        ) : undefined
-      }
-      dictionary={initDictionary}
-      cellParams={cellParams}
-      onClick={handleClick}
-      hiddenColumnNames={['ngduId']}
-    />
+    <Card style={{ padding: 0 }}>
+      <Table
+        localStorageKey={localStorageKey}
+        height={height}
+        acrossLine={acrossLine}
+        dataSource={dataResponse}
+        columns={columns}
+        sticky={sticky}
+        skeleton={
+          loading ? (
+            <Skeleton
+              rows={16}
+              columns={[2, 3, 5, 10, 3, 16, 6, 9, 9, 7, 8, 10, 10]}
+              columnsUnit="fr"
+              isTable
+            />
+          ) : undefined
+        }
+        dictionary={initDictionary}
+        cellParams={cellParams}
+        onClick={handleClick}
+        hiddenColumnNames={hiddenColumnNames}
+      />
+    </Card>
   );
 };
 
 export const Default: Story = {
   render: ({ ...args }) => <TableStory {...args} />,
   args: {
+    localStorageKey: 'sb-default',
     sticky: true,
     cellParams: {
       verticalClamp: 3,
     },
-    height: 'calc(100vh - 16px)',
+    height: 'calc(100vh - 32px)',
     acrossLine: false,
+    hiddenColumnNames: ['ngduId'],
+  },
+};
+
+export const SmallTable: Story = {
+  render: ({ ...args }) => <TableStory {...args} />,
+  args: {
+    localStorageKey: 'sb-small',
+    sticky: true,
+    cellParams: {
+      verticalClamp: 3,
+    },
+    height: 'calc(100vh - 32px)',
+    acrossLine: true,
+    hiddenColumnNames: [
+      'year',
+      'fieldId',
+      'ngduId',
+      'investmentDate',
+      'coordinateX',
+      'coordinateY',
+      'landAllocationDuration',
+      'idd',
+      'netPresentValue',
+      'payBackTime',
+      'sitePreparationDuration',
+      'iddCoeff',
+      'netPresentValueCoeff',
+    ],
   },
 };
