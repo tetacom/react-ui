ca46cbdfa97b0207ac917fed5468403d7f7e2170
andy <andreyelpaev@gmail.com>
2023-12-12 10:20:43 +0500

    fix: add name of axis, add dropdown maxheightcontainer prop

--- react-components/src/components/Chart/components/Axis/y-axis.tsx
+++ react-components/src/components/Chart/components/Axis/y-axis.tsx
@@ -1,23 +1,41 @@
 import { Axis } from '../../useScales';
+import s from './style.module.scss';
 
-export type AxisProps = Pick<Axis, 'scale'>;
+export type AxisProps = Pick<Axis, 'scale' | 'title' | 'size'>;
 
 export function YAxis(props: AxisProps) {
   const { scale } = props;
 
   if (!scale) return null;
 
-  return scale.ticks().map((tick: number | Date) => (
-    <g
-      key={tick.toString()}
-      transform={`translate(0, ${scale(tick)})`}
-      textAnchor="end"
-      style={{ shapeRendering: 'crispEdges' }}
-    >
-      <line stroke="var(--color-text-10)" x2="-6"></line>
-      <text fontSize={11} fill="var(--color-text-90)" dy="0.32em" dx="-9">
-        {tick instanceof Date ? tick.getMilliseconds() : tick}
-      </text>
-    </g>
-  ));
+  return (
+    <>
+      <g>
+        <text
+          className={s.label}
+          transform={`translate(${-props.size}, ${
+            props.scale.range()[1] / 2
+          }) rotate(-90)`}
+          dy={6}
+          text-anchor="middle"
+          dominant-baseline="middle"
+        >
+          {props.title}
+        </text>
+      </g>
+      {scale.ticks().map((tick: number | Date) => (
+        <g
+          key={tick.toString()}
+          transform={`translate(0, ${scale(tick)})`}
+          textAnchor="end"
+          style={{ shapeRendering: 'crispEdges' }}
+        >
+          <line stroke="var(--color-text-10)" x2="-6"></line>
+          <text fontSize={11} fill="var(--color-text-90)" dy="0.32em" dx="-9">
+            {tick instanceof Date ? tick.getMilliseconds() : tick}
+          </text>
+        </g>
+      ))}
+    </>
+  );
 }
